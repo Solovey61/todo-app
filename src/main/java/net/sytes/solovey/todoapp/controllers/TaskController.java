ac1f7de21bf5d6c1e7c771b605c1b175c836449f
Michael Solovey <solovey.0408@gmail.com>
2019-12-09 12:40:46 +0300

    Errors handling changes

--- src/main/java/net/sytes/solovey/todoapp/controllers/TaskController.java
+++ src/main/java/net/sytes/solovey/todoapp/controllers/TaskController.java
@@ -1,51 +1,61 @@
 package net.sytes.solovey.todoapp.controllers;
 
-import net.sytes.solovey.todoapp.exceptions.NotFoundException;
 import net.sytes.solovey.todoapp.models.Task;
 import net.sytes.solovey.todoapp.repos.TaskRepo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/task")
 public class TaskController {
-	private final TaskRepo taskRepo;
+    private final TaskRepo taskRepo;
 
-	@Autowired
-	public TaskController(TaskRepo taskRepo) {
-		this.taskRepo = taskRepo;
-	}
+    @Autowired
+    public TaskController(TaskRepo taskRepo) {
+        this.taskRepo = taskRepo;
+    }
 
-	@GetMapping
-	public List<Task> getTasksList() {
-		return taskRepo.findAll();
-	}
+    @GetMapping
+    public List<Task> getTasksList() {
+        return taskRepo.findAll();
+    }
 
-	@GetMapping("{id}")
-	public Task getTask(@PathVariable("id") Integer id) {
-		return taskRepo.findById(id)
-				.orElseThrow(() -> new NotFoundException(id));
-	}
+    @GetMapping("{id:\\d+}")
+    public ResponseEntity getTask(@PathVariable("id") Integer id) {
+        Optional<Task> task = taskRepo.findById(id);
+        return task.map(t -> new ResponseEntity<>(t, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity("NOT FOUND", HttpStatus.NOT_FOUND));
+    }
 
-	@PostMapping
-	public Task addNewTask(@RequestBody Task task) {
-		return taskRepo.save(task);
-	}
+    @PostMapping
+    public Task addNewTask(@RequestBody Task task) {
+        task.setId(null);
+        return taskRepo.save(task);
+    }
 
-	@PutMapping("{id}")
-	public Task updateTask(@PathVariable("id") Integer id, @RequestBody Task task) {
-		Task taskFromDb = taskRepo.findById(id)
-				.orElseThrow(() -> new NotFoundException(id));
-		BeanUtils.copyProperties(task, taskFromDb, "id", "creationDate");
-		return taskRepo.save(taskFromDb);
-	}
+    @PutMapping("{id:\\d+}")
+    public ResponseEntity updateTask(@PathVariable("id") Integer id, @RequestBody Task task) {
+        Optional<Task> taskFromDb = taskRepo.findById(id);
+        return taskFromDb.map(t -> {
+            BeanUtils.copyProperties(task, t, "id", "creationDate");
+            return new ResponseEntity<>(taskRepo.save(t), HttpStatus.OK);
+        })
+                .orElseGet(() -> new ResponseEntity("NOT FOUND", HttpStatus.NOT_FOUND));
+    }
 
-	@DeleteMapping("{id}")
-	public void deleteTask(@PathVariable("id") Integer id) throws NotFoundException {
-		taskRepo.delete(taskRepo.findById(id)
-				.orElseThrow(() -> new NotFoundException(id)));
-	}
+    @DeleteMapping("{id:\\d+}")
+    public ResponseEntity deleteTask(@PathVariable("id") Integer id) {
+        Optional<Task> task = taskRepo.findById(id);
+        return task.map(t -> {
+            taskRepo.delete(t);
+            return new ResponseEntity("OK", HttpStatus.OK);
+        })
+                .orElseGet(() -> new ResponseEntity("NOT FOUND", HttpStatus.NOT_FOUND));
+    }
 }
