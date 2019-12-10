75a8d891845af45a353bf23a460f956bac254138
Michael Solovey <solovey.0408@gmail.com>
2019-12-10 14:11:56 +0300

    Code review

--- src/main/java/net/sytes/solovey/todoapp/controllers/TaskController.java
+++ src/main/java/net/sytes/solovey/todoapp/controllers/TaskController.java
@@ -1,10 +1,10 @@
 package net.sytes.solovey.todoapp.controllers;
 
+import net.sytes.solovey.todoapp.APIResponse;
 import net.sytes.solovey.todoapp.models.Task;
 import net.sytes.solovey.todoapp.repos.TaskRepo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +15,8 @@ import java.util.Optional;
 @RequestMapping("api/task")
 public class TaskController {
     private final TaskRepo taskRepo;
+    private final String RESPONSE_NOT_FOUND_MESSAGE = "NOT FOUND";
+    private final String RESPONSE_OK_MESSAGE = "OK";
 
     @Autowired
     public TaskController(TaskRepo taskRepo) {
@@ -27,35 +29,38 @@ public class TaskController {
     }
 
     @GetMapping("{id:\\d+}")
-    public ResponseEntity getTask(@PathVariable("id") Integer id) {
+    public ResponseEntity<Object> getTask(@PathVariable("id") Integer id) {
         Optional<Task> task = taskRepo.findById(id);
-        return task.map(t -> new ResponseEntity<>(t, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity("NOT FOUND", HttpStatus.NOT_FOUND));
+        return task.isPresent()
+                ? APIResponse.getAPIOkResponse(task.get())
+                : APIResponse.getAPINotFoundResponse(RESPONSE_NOT_FOUND_MESSAGE);
     }
 
     @PostMapping
-    public Task addNewTask(@RequestBody Task task) {
+    public ResponseEntity<Object> addNewTask(@RequestBody Task task) {
         task.setId(null);
-        return taskRepo.save(task);
+        taskRepo.save(task);
+        return APIResponse.getAPIOkResponse(task);
     }
 
     @PutMapping("{id:\\d+}")
-    public ResponseEntity updateTask(@PathVariable("id") Integer id, @RequestBody Task task) {
+    public ResponseEntity<Object> updateTask(@PathVariable("id") Integer id, @RequestBody Task task) {
         Optional<Task> taskFromDb = taskRepo.findById(id);
-        return taskFromDb.map(t -> {
-            BeanUtils.copyProperties(task, t, "id", "creationDate");
-            return new ResponseEntity<>(taskRepo.save(t), HttpStatus.OK);
-        })
-                .orElseGet(() -> new ResponseEntity("NOT FOUND", HttpStatus.NOT_FOUND));
+        if (taskFromDb.isPresent()) {
+            BeanUtils.copyProperties(task, taskFromDb.get(), "id", "creationDate");
+            taskRepo.save(taskFromDb.get());
+            return APIResponse.getAPIOkResponse(taskFromDb.get());
+        }
+        return APIResponse.getAPINotFoundResponse(RESPONSE_NOT_FOUND_MESSAGE);
     }
 
     @DeleteMapping("{id:\\d+}")
-    public ResponseEntity deleteTask(@PathVariable("id") Integer id) {
+    public ResponseEntity<Object> deleteTask(@PathVariable("id") Integer id) {
         Optional<Task> task = taskRepo.findById(id);
-        return task.map(t -> {
-            taskRepo.delete(t);
-            return new ResponseEntity("OK", HttpStatus.OK);
-        })
-                .orElseGet(() -> new ResponseEntity("NOT FOUND", HttpStatus.NOT_FOUND));
+        if (task.isPresent()) {
+            taskRepo.delete(task.get());
+            return APIResponse.getAPIOkResponse(RESPONSE_OK_MESSAGE);
+        }
+        return APIResponse.getAPINotFoundResponse(RESPONSE_NOT_FOUND_MESSAGE);
     }
 }
